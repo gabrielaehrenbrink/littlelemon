9c3fef21770bb2cc27a51546f53d62fa426f9cfe
Gabriela <gabbype@gmail.com>
2024-05-30 10:28:19 +0100

    components created, index.html meta tags added and components added to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import Header from './Components/Header';
+import Nav from './Components/Nav';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/Components/Main.js
@@ -0,0 +1,9 @@
+function Main() {
+    return (
+      <main>
+        {/* Add your main content here */}
+      </main>
+    );
+  }
+  
+  export default Main;
